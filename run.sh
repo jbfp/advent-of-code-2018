22557af27b76ca0d38e4f4e84440e8bd1c5255b3
jbfp <jakobp10@gmail.com>
2018-12-06 16:56:54 +0100

    Add 'time' command to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 echo "Part 1"
-dotnet-script ./part-01.csx
+time dotnet-script ./part-01.csx
 
 echo ""
 
 echo "Part 2"
-dotnet-script ./part-02.csx
+time dotnet-script ./part-02.csx
+
